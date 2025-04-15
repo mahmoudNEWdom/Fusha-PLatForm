cbea3b725feab506b9ebc6f4bc3fcafff2bb9435
mahmoudNEWdom <pprokiller2@gmail.com>
2025-04-16 00:02:49 +0200

    Update next.config.mjs to enhance configuration for static export and image handling

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,13 @@ const nextConfig = {
     styledComponents: true,
     emotion: true,
   },
+  reactStrictMode: true,
+  swcMinify: true,
+  output: "export", // Enable static export
+  basePath: process.env.NODE_ENV === "production" ? "/Fusha-Platform" : "", // Add GitHub repo name as base path
+  trailingSlash: true, // Add trailing slashes to URLs for static hosting
   images: {
+    unoptimized: true, // Required for static export
     remotePatterns: [
       {
         protocol: "https",
